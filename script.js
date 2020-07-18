fd5d9dc0b699f1a28b5e731281d843a157e31c08
devanshi-katyal <devanshi.katyal09@gmail.com>
2020-07-19 00:49:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+$("document").ready(function () {
+   $("#change").click(function () {
+    $("#innerve").text("INNERVE");
+    $("#getset").text("GET SET INNOVATE");
+    $("#reg").text("Registration is mandatory for entry into the IGDTUW campus.Participation in events requires an additional registration.Please navigate to the Events page for the same.");
+    $("#about").text("About Us");
+    $("#aboutm").text("Innerve is the annual technical fest of Indira Gandhi Delhi Technical University for Women (IGDTUW). With its inception in 2015, every year, the two-day extravaganza has witnessed a phenomenol gathering of students from all domains. The event is back for its SIXTH edition where it aims to be a platform for participants to test their engineering skills and showcase their prowess.");
+    $("#get").text("Get in touch");
+    $("#para").text("College Address :Madrasa Road Opposite St. James Church <br>  Kashmere Gate, New Delhi, Delhi 110006");
+    $("#email").text("Email:sample@gmail.com");
+    $("#innerve").text("INNERVE");
+   } );
+ })
